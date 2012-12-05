05225d2001ed30cb4093dd4866d1755563b6f081
Víctor Terrón <vterron@iaa.es>
2012-12-05 10:23:38 +0100

    Add unit test for Pixel.__init__

---
+++ test/test_astromatic.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+# Copyright (c) 2012 Victor Terron. All rights reserved.
+# Institute of Astrophysics of Andalusia, IAA-CSIC
+#
+# This file is part of LEMON.
+#
+# LEMON is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import random
+import unittest
+
+from astromatic import Pixel
+
+NITERS = 100
+
+class PixelTest(unittest.TestCase):
+
+    X_COORD_RANGE = (1, 2048)
+    Y_COORD_RANGE = (1, 2048)
+
+    def test_init(self):
+        for _ in xrange(NITERS):
+            x = random.uniform(*self.X_COORD_RANGE)
+            y = random.uniform(*self.Y_COORD_RANGE)
+            pixel = Pixel(x, y)
+            self.assertEqual(pixel.x, x)
+            self.assertEqual(pixel.y, y)
+
