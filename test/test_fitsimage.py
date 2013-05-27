8dceddc08c72a7f077a751bbb944bfa2a504adbe
Víctor Terrón <vterron@iaa.es>
2013-05-27 11:33:13 +0200

    Add FITSImageTest.random() class method

--- test/test_fitsimage.py
+++ test/test_fitsimage.py
@@ -21,13 +21,38 @@
 import numpy.random
 import os
 import pyfits
+import random
 import tempfile
 import unittest
+
+# LEMON modules
+import fitsimage
+
+class FITSTestImage(fitsimage.FITSImage):
+    """ Delete FITSImage on exit from the body of the with statement.
+
+    This class does not alter the functionality of the fitsimage.FITSImage
+    class. The only difference is that it adds support for the with statement
+    (the __enter__() and __exit__() magic methods), deleting the FITS file on
+    exit from the body of the with statement. In this manner, we can focus on
+    writing our unit tests without having to worry about deleting the temporary
+    FITS files that we use.
+
+    """
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.unlink()
+
 
 class FITSImageTest(unittest.TestCase):
 
     MIN_PIXEL_VALUE = 0
     MAX_PIXEL_VALUE = 65535
+    MIN_SIZE = 100
+    MAX_SIZE = 2048
 
     @classmethod
     def mkfits(cls, x_size, y_size, **keywords):
@@ -57,4 +82,22 @@ class FITSImageTest(unittest.TestCase):
         os.close(fd)
         hdu.writeto(path)
         return path
+
+    @classmethod
+    def random(cls, **keywords):
+        """ Return a random, temporary FITS image.
+
+        This method returns a temporary FITS image (see FITSImageTest.mkfits),
+        encapsulated as a FITSTestImage object -- which means that the file is
+        automatically deleted on exit from the body of a with statement. The
+        dimensions of the FITS image along the x- and y- axes are random
+        numbers in the range [MIN_SIZE, MAX_SIZE]. Keyword/value pairs are
+        passed down to mkfits() and stored in the header of the FITS image.
+
+        """
+
+        x_size = random.randint(cls.MIN_SIZE, cls.MAX_SIZE)
+        y_size = random.randint(cls.MIN_SIZE, cls.MAX_SIZE)
+        path = cls.mkfits(x_size, y_size, **keywords)
+        return FITSTestImage(path)
 
