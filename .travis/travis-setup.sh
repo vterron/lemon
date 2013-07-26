bf966b1d21171562b28311e0ad5793df39b0734e
Víctor Terrón <vterron@iaa.es>
2013-07-26 14:11:44 +0200

    Add support for 32-bit machines
    Travis CI virtual machines are based on Ubuntu 12.04 LTS Server
    Edition 64 bit, but this change allows us to test the Bash script,
    without having to commit and push to GitHub, in 32-bits systems too.

--- .travis/travis-setup.sh
+++ .travis/travis-setup.sh
@@ -4,6 +4,18 @@
 # Author: Victor Terron (c) 2013
 # License: GNU GPLv3
 
+REGEXP=".*64$"
+HARDWARE_NAME=`uname -m`
+
+echo -n "Machine architecture: "
+if [[ $HARDWARE_NAME =~ $REGEXP ]]; then
+    ARCH_64_BITS=1;
+    echo "64-bit"
+else
+    ARCH_64_BITS=0;
+    echo "32-bit"
+fi
+
 CWD=$(pwd)
 
 cd ~
@@ -22,7 +34,11 @@ pip install pyraf
 ########### Install IRAF ################
 
 DOWNLOADS_SERVER="http://iaa.es/lemon/travis/"
-IRAF_TAR="iraf.lnux.x86_64.tar.gz"
+if [[ $ARCH_64_BITS == 1 ]]; then
+    IRAF_TAR="iraf.lnux.x86_64.tar.gz"
+else
+    IRAF_TAR="iraf.lnux.x86.tar.gz"
+fi
 IRAF_URL=$DOWNLOADS_SERVER$IRAF_TAR
 
 IRAF_DIR="/iraf/iraf"
@@ -55,9 +71,15 @@ make install
 
 cd ~
 
-SEXTRACTOR_RPM="sextractor-2.8.6-1.x86_64.rpm"
-SCAMP_RPM="scamp-1.7.0-1.x86_64.rpm"
-SWARP_RPM="swarp-2.19.1-1.x86_64.rpm"
+if [[ $ARCH_64_BITS == 1 ]]; then
+    SEXTRACTOR_RPM="sextractor-2.8.6-1.x86_64.rpm"
+    SCAMP_RPM="scamp-1.7.0-1.x86_64.rpm"
+    SWARP_RPM="swarp-2.19.1-1.x86_64.rpm"
+else
+    SEXTRACTOR_RPM="sextractor-2.8.6-1.i386.rpm"
+    SCAMP_RPM="scamp-1.7.0-1.i386.rpm"
+    SWARP_RPM="swarp-2.19.1-1.i386.rpm"
+fi
 
 SEXTRACTOR_URL=$DOWNLOADS_SERVER$SEXTRACTOR_RPM
 SCAMP_URL=$DOWNLOADS_SERVER$SCAMP_RPM
