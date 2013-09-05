e18f285835a9f4f96fe4c650c067fffbeef49084
Víctor Terrón <vterron@iaa.es>
2013-09-05 13:21:20 +0200

    Add str_to_version() module-level function

--- check_versions.py
+++ check_versions.py
@@ -4,3 +4,7 @@ def version_to_str(version):
     """ From (0, 2, 4) to '0.2.4' for example """
     return '.'.join(str(x) for x in version)
 
+def str_to_version(version):
+    """ From '0.2.4' to (0, 2, 4), for example """
+    return tuple(int(x) for x in version.split('.'))
+
