7c921cc687514b595b46cb8257b21615bddab858
Víctor Terrón <vterron@iaa.es>
2013-11-12 11:43:07 +0100

    Remove exception SCAMPNotInstalled

--- astromatic.py
+++ astromatic.py
@@ -63,9 +63,6 @@ class SExtractorUpgradeRequired(StandardError):
 class SExtractorError(subprocess.CalledProcessError):
     pass
 
-class SCAMPNotInstalled(StandardError):
-    pass
-
 class SCAMPError(subprocess.CalledProcessError):
     pass
 
