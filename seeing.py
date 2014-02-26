edf1198cc74ff23298bc9dda4b4a177ee5b86c65
Víctor Terrón <vterron@iaa.es>
2014-02-26 14:04:31 +0100

    Add 'saturk' keyword argument to FITSeeingImage.__init__()

--- seeing.py
+++ seeing.py
@@ -67,7 +67,8 @@ class FITSeeingImage(fitsimage.FITSImage):
 
     """
 
-    def __init__(self, path, maximum, margin, coaddk = keywords.coaddk):
+    def __init__(self, path, maximum, margin,
+                 coaddk = keywords.coaddk, saturk = keywords.saturk):
         """ Instantiation method for the FITSeeingImage class.
 
         The path to the SExtractor catalog is read from the FITS header: if the
@@ -205,7 +206,7 @@ class FITSeeingImage(fitsimage.FITSImage):
                     # about "illegal values" if it receives a Unicode string.
                     self.update_keyword(keywords.sex_catalog, str(self.catalog_path))
                     self.update_keyword(keywords.sex_md5sum, sex_md5sum)
-                    args = keywords.saturk, self.saturation
+                    args = saturk, self.saturation
                     self.update_keyword(*args, comment = satur_comment)
                 except IOError:
                     pass
