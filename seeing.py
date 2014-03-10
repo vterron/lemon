d494325e8e6236f0c2de0eb0bdaaf1ffc9de03b3
Víctor Terrón <vterron@iaa.es>
2014-03-10 13:41:46 +0100

    Update docstring of FITSeeingImage.__init__()

--- seeing.py
+++ seeing.py
@@ -82,9 +82,7 @@ class FITSeeingImage(fitsimage.FITSImage):
         example, the effective saturation level equals three times the value of
         'saturation'. The number of effective coadds is read from the 'coaddk'
         parameter. If the keyword is missing, a value of one (that is, that the
-        image consisted of a single exposure) is assumed. The effective
-        saturation level, in ADUs, is written to the FITS header with the
-        keyword 'saturk'.
+        image consisted of a single exposure) is assumed.
 
         The 'margin' argument gives the width, in pixels, of the areas adjacent
         to the edges of the image that are to be ignored when detecting sources
