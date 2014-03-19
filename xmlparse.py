75f0b734c14873fa55207457d2612224c43fd4ca
Víctor Terrón <vterron@iaa.es>
2014-03-19 13:57:58 +0100

    Remove unused imports

--- xmlparse.py
+++ xmlparse.py
@@ -18,11 +18,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import calendar
 import collections
 import lxml.etree
 import operator
-import time
 
 # LEMON modules
 import methods
