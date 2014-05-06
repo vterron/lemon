38d86728e425266077a0371f1290b1ef224b5384
Víctor Terrón <vterron@iaa.es>
2014-05-06 14:13:10 +0200

    Add SharedCounter class

--- methods.py
+++ methods.py
@@ -22,6 +22,7 @@ import contextlib
 import functools
 import logging
 import math
+import multiprocessing
 import numpy
 import os
 import os.path
@@ -553,3 +554,31 @@ def clean_tmp_files(*paths):
             else:
                 msg = "Temporary file '%s' removed"
                 logging.debug(msg % path)
+
+class SharedCounter(object):
+    """ A synchronized shared counter.
+
+    The locking done by multiprocessing.Value ensures that only a single
+    process or thread may read or write the in-memory ctypes object. However,
+    in order to do n += 1, Python performs a read followed by a write, so a
+    second process may read the old value before the new one is written by the
+    first process. The solution is to use a multiprocessing.Lock to guarantee
+    the atomicity of the modifications to Value.
+
+    This class comes almost entirely from Eli Bendersky's blog:
+    http://eli.thegreenplace.net/2012/01/04/shared-counter-with-pythons-multiprocessing/
+
+    """
+
+    def __init__(self, n = 0):
+        self.count = multiprocessing.Value('i', n)
+
+    def increment(self, n = 1):
+        """ Increment the counter by n (default = 1) """
+        with self.count.get_lock():
+            self.count.value += n
+
+    @property
+    def value(self):
+        """ Return the value of the counter """
+        return self.count.value
