c2e33894da54834c89a8515446f8f7527ecc6ad7
Víctor Terrón <vterron@iaa.es>
2014-05-26 10:45:28 +0200

    Add LoadCoordinatesTest class

---
+++ test/test_methods.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+# Copyright (c) 2014 Victor Terron. All rights reserved.
+# Institute of Astrophysics of Andalusia, IAA-CSIC
+#
+# This file is part of LEMON.
+#
+# LEMON is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import division
+
+# LEMON modules
+from test import unittest
+from astromatic import Coordinates
+
+class LoadCoordinatesTest(unittest.TestCase):
+
+    # A series of two-element tuples, one per line. The first element is a
+    # string containing the name of an astronomical object. The second is
+    # another two-element tuple with its sky coordinates (right ascension
+    # and declination), in decimal degrees.
+    TEST_DATA_DIR = './test/test_data/SIMBAD_objects'
+
+    # Parse the SIMBAD file and map each astronomical object (a string) to
+    # its right ascension and declination (an astromatic.Coordinates object)
+    COORDINATES = {}
+    with open(TEST_DATA_DIR, 'rt') as fd:
+        for line in fd:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                object_, coords = eval(line)
+                COORDINATES[object_] = Coordinates(*coords)
+
