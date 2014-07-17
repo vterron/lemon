0ffc71e559889f043b5cef6ec67c5b3f1806a72b
Víctor Terrón <vterron@iaa.es>
2014-07-17 14:06:20 +0200

    Update usage message

--- periods.py
+++ periods.py
@@ -225,7 +225,7 @@ class StringLengthFinder(object):
 
 
 parser = customparser.get_parser(description)
-parser.usage = "%prog [OPTION]... INPUT_DB"
+parser.usage = "%prog [OPTION]... INPUT_DB OUTPUT_DB"
 
 parser.add_option('--overwrite', action = 'store_true', dest = 'overwrite',
                   help = "overwrite output database if it already exists")
