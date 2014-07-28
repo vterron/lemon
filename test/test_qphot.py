9977a16148fe4906bef894ddc8ed9717ac5bc676
Víctor Terrón <vterron@iaa.es>
2014-07-28 17:51:27 +0200

    Add unit test for qphot.run()
    This is a test that should have been written a long time ago, and that
    would have saved us some time trying to figure out what was failing in
    issue #23. Test coverage of qphot.run() is not yet complete, but with
    this we can finally verify that PyRAF and IRAF's qphot are working as
    expected when we try to do photometry.

--- test/test_qphot.py
+++ test/test_qphot.py
@@ -22,6 +22,13 @@ import os.path
 import pyfits
 import tempfile
 
+# LEMON modules
+from test import unittest
+import fitsimage
+import methods
+import qphot
+import test.test_fitsimage
+
 def fix_DSS_image(path):
     """ Fix images downloaded from STScI DSS before we can do photometry.
 
@@ -51,4 +58,54 @@ def fix_DSS_image(path):
         hdu.writeto(output_path)
 
     return output_path
+
+
+class QPhotTest(unittest.TestCase):
+
+    def test_qphot_run(self):
+
+        # A simple test: do photometry on the DSS image of NGC 2264, measuring
+        # ten astronomical objects whose celestial coordinates (right ascension
+        # and declination) we know. Then, compare the photometric measurements,
+        # returned as qphot.QPhotResult objects, to the expected values.
+
+        ngc2264_path = './test/test_data/fits/NGC_2264.fits'
+        ngc2264_input_coords = (
+            "100.1191901 9.8177770\n"
+            "100.1543316 9.7909363\n"
+            "100.1597762 9.7878795\n"
+            "100.1598790 9.9627296\n"
+            "100.2147546 9.8636567\n"
+            "100.2446191 9.8962391\n"
+            "100.2502955 9.8714701\n"
+            "100.2579343 9.8802548\n"
+            "100.2933265 9.8838196\n"
+            "100.3635468 9.8540181\n")
+
+        ngc2264_expected_output = (
+            #                 x        y        mag     sum      flux     stdev
+            qphot.QPhotResult(878.62,  171.496, 17.623, 6266626, 3749740, 484.9941),
+            qphot.QPhotResult(755.241, 75.308,  17.821, 6015410, 3124231, 579.0784),
+            qphot.QPhotResult(736.138, 64.345,  17.777, 6035459, 3254578, 572.4526),
+            qphot.QPhotResult(734.601, 689.319, 17.631, 6102620, 3723123, 555.338),
+            qphot.QPhotResult(542.399, 334.835, 18.01,  5197306, 2624735, 374.3998),
+            qphot.QPhotResult(437.227, 451.111, 18.54,  9239881, 1611601, 3583.002),
+            qphot.QPhotResult(417.419, 362.544, 18.305, 4541155, 2001931, 362.4446),
+            qphot.QPhotResult(390.527, 393.897, 17.768, 5925938, 3280150, 450.3236),
+            qphot.QPhotResult(266.116, 406.437, 18.12,  4804557, 2372792, 378.5256),
+            qphot.QPhotResult(19.449,  299.555, 17.409, 7143799, 4567058, 540.6945))
+
+        kwargs = dict(aperture = 11,
+                      annulus  = 13,
+                      dannulus = 8,
+                      maximum = 30000,
+                      exptimek = 'EXPOSURE',
+                      uncimgk = None)
+
+        path = fix_DSS_image(ngc2264_path)
+        with test.test_fitsimage.FITSImage(path) as img:
+            with methods.tempinput(ngc2264_input_coords) as coords_path:
+                result = qphot.run(img, coords_path, **kwargs)
+                for phot, expected_phot in zip(result, ngc2264_expected_output):
+                    self.assertEqual(phot, expected_phot)
 
