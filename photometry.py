2ea430fad665a65235f6283eb55ef078cfebb5de
Víctor Terrón <vterron@iaa.es>
2014-09-03 15:31:59 +0200

    Discard images not taken in any of the given --filters

--- photometry.py
+++ photometry.py
@@ -746,34 +746,36 @@ def main(arguments = None):
 
     # The --filter option allows the user to specify on which FITS files, among
     # all those received as input, photometry must be done: only those files in
-    # the options.filter photometric filter. The Passband class, which supports
-    # comparison operations, makes it possible to compare filters for what they
-    # really are, not how they were written: "Johnson V" and "johnson_v", for
-    # example, are the same filter after all, but if we just compared the two
-    # strings we would consider them to be different.
-    if options.filter:
+    # any of the photometric filters contained in options.filter. The Passband
+    # class, which supports comparison operations, makes it possible to compare
+    # filters for what they really are, not how they were written: "Johnson V"
+    # and "johnson_v", for example, are the same filter after all, but if we
+    # just compared the two strings we would consider them to be different.
 
-        msg = "%sIgnoring images not taken in the '%s' photometric filter..."
-        print msg % (style.prefix, options.filter) ,
+    if options.filters:
+
+        msg = "%sIgnoring images not taken in any of the following filters:"
+        print msg % style.prefix
+        for index, pfilter in enumerate(sorted(options.filters)):
+            print "%s (%d) %s" % (style.prefix, index + 1, pfilter)
         sys.stdout.flush()
 
         discarded = 0
         for pfilter, images in files.items():
-            if pfilter != options.filter:
+            if pfilter not in options.filters:
                 discarded += len(images)
                 del files[pfilter]
 
         if not files:
             print
-            msg = "%sError. No image was taken in the '%s' filter."
-            print msg % (style.prefix, options.filter)
+            msg = "%sError. No image was taken in any of the above filters."
+            print msg % style.prefix
             print style.error_exit_message
             return 1
 
         else:
-            print 'done.'
-            msg = "%s%d images taken in the '%s' filter, %d were discarded."
-            print msg % (style.prefix, len(files), options.filter, discarded)
+            msg = "%s%d images taken in the above filters, %d were discarded."
+            print msg % (style.prefix, len(files), discarded)
 
     # If a JSON file is specified with --annuli, it must list the photometric
     # parameters for all the filters on which photometry is to be done.
