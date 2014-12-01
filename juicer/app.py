c697eabe2f7052d4f127470df2b01639f8b3c2d5
Víctor Terrón <vterron@iaa.es>
2014-12-01 13:22:16 +0100

    Write value of the checkbox to the configuration file when toggled

--- juicer/app.py
+++ juicer/app.py
@@ -170,6 +170,7 @@ class ExportCurveDialog(object):
         self.snr_checkbox.connect('toggled', f('dump_snr'))
         self.merr_pos_checkbox.connect('toggled', f('dump_max_merr'))
         self.merr_neg_checkbox.connect('toggled', f('dump_min_merr'))
+        self.inst_mags_checkbox.connect('toggled', f('dump_instrumental_magnitude'))
         self.spinbutton.connect('output', f('decimal_places', 'get_value'))
 
     def update(self):
