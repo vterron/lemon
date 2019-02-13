c95b3b01752cb6b30c732a415786c7121ec4f2ce
Víctor Terrón <quintanar@gmail.com>
2019-02-13 23:04:47 +0000

    Add unit tests for all Queue methods

--- test/test_methods.py
+++ test/test_methods.py
@@ -375,22 +375,83 @@ class StreamToWarningFilterTest(unittest.TestCase):
 
 class MethodsQueueTest(unittest.TestCase):
 
-    def test_queue_min_size(self):
-        q = methods.Queue()
-        self.assertEqual(q.qsize(), 0)
-        with self.assertRaises(Queue.Empty):
-            q.get_nowait()
-        self.assertEqual(q.qsize(), 0)
+    def test_put_and_get(self):
+        """Test that this is a FIFO queue."""
 
-    def test_queue_max_size(self):
+        q = methods.Queue()
+        q.put(5)
+        q.put(7)
+        q.put(9)
+
+        self.assertEqual(5, q.get())
+        self.assertEqual(7, q.get())
+        self.assertEqual(9, q.get())
+
+    def test_put_when_queue_is_full(self):
+        """Test that if Queue.Full is raised, qsize() doesn't break."""
+
         q = methods.Queue(maxsize=1)
-        self.assertEqual(0, q.qsize())
-        q.put_nowait("1")
+        q.put(5)
         self.assertEqual(1, q.qsize())
         with self.assertRaises(Queue.Full):
-            q.put_nowait("1")
+            q.put(7, block=False)
+
+        # Internal counter doesn't change, see:
+        # https://github.com/vterron/lemon/pull/103
         self.assertEqual(1, q.qsize())
 
         # Pause to allow queue to complete operation before terminating
         # Otherwise sometimes results in 'IOError: [Errno 32] Broken pipe'
         time.sleep(0.1)
+
+    def test_get_when_queue_is_empty(self):
+        """Test that if Queue.Empty is raised, qsize() doesn't break."""
+
+        q = methods.Queue()
+        self.assertEqual(0, q.qsize())
+        with self.assertRaises(Queue.Empty):
+            q.get(block=False)
+
+        # Internal counter doesn't change, see:
+        # https://github.com/vterron/lemon/pull/103
+        self.assertEqual(0, q.qsize())
+        time.sleep(0.1)
+
+    def test_qsize(self):
+        """Test that qsize() gets updated correctly as we put() and get()."""
+
+        q = methods.Queue()
+        self.assertEqual(0, q.qsize())
+        q.put(5)
+        self.assertEqual(1, q.qsize())
+        q.put(7)
+        self.assertEqual(2, q.qsize())
+        q.put(9)
+        self.assertEqual(3, q.qsize())
+
+        q.get()
+        self.assertEqual(2, q.qsize())
+        q.get()
+        self.assertEqual(1, q.qsize())
+        q.get()
+        self.assertEqual(0, q.qsize())
+
+    def test_empty(self):
+        """Test empty()."""
+
+        q = methods.Queue()
+        self.assertTrue(q.empty())
+        q.put(1)
+        self.assertFalse(q.empty())
+        time.sleep(0.1)
+
+    def test_clear(self):
+        """Test that clear() empties the queue."""
+
+        q = methods.Queue()
+        q.put(5)
+        q.put(7)
+        q.put(9)
+
+        q.clear()
+        self.assertEqual(0, q.qsize())
