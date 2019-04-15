2d23362597ccd2c62725ced2f4b72a05d9eef9c1
dokeeffe <derek0207@gmail.com>
2019-04-15 11:31:38 +0100

    Small improvements from code review

--- test/test_methods.py
+++ test/test_methods.py
@@ -158,19 +158,22 @@ class LoadCoordinatesTest(unittest.TestCase):
             coordinates = methods.load_coordinates(path)
             coords_list = list(coordinates)
             self.assertEqual(len(coords_list), 1)
-            self.assertEqual(coords_list[0], (7.9720694373e-05, 44.6352243008, None, None))
+            ra, dec, pm_ra, pm_dec = coords_list[0]
+            self.assertAlmostEqual(ra, 7.9720694373e-05)
+            self.assertAlmostEqual(dec, 44.6352243008)
 
     def test_load_coordinates_scientific_notation_with_propper_motion(self):
-
-        # For some datasets that generate coords so close to zero that they end up in scientific notation
 
         data = '7.9720694373e-05 44.6352243008 [0.00123] [0.0000432]'
         with methods.tempinput(data) as path:
             coordinates = methods.load_coordinates(path)
             coords_list = list(coordinates)
             self.assertEqual(len(coords_list), 1)
-            self.assertEqual(coords_list[0], (7.9720694373e-05, 44.6352243008, 0.00123, 4.32e-05))
-
+            ra, dec, pm_ra, pm_dec = coords_list[0]
+            self.assertAlmostEqual(ra, 7.9720694373e-05)
+            self.assertAlmostEqual(dec, 44.6352243008)
+            self.assertAlmostEqual(pm_ra, 0.00123)
+            self.assertAlmostEqual(pm_dec, 4.32e-05)
 
     def test_load_coordinates_empty_lines_and_comments(self):
 
