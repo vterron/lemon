199b96201b29ce1f67bbd9a379154e150983bae2
Víctor Terrón <quintanar@gmail.com>
2019-10-20 18:20:07 +0000

    Sort imports lexicographically

--- util/__init__.py
+++ util/__init__.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 
-from .queue import *
+from .context import *
 from .coords import *
 from .display import *
 from .io import *
-from .memoize import *
-from .context import *
 from .log import *
+from .memoize import *
+from .queue import *
